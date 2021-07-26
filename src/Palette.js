451b56d7111dc31df3b227ee8553fb3b3de54940
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-26 11:42:42 +0200

    created component for Palette

---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Palette extends Component {
+    render() {
+        return (
+            <div className="Palette">
+                <h1>Here comes the palette</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Palette
