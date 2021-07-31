b7495242ba211b4ffc2d929fb768d302c1f7cd19
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-07-31 12:36:00 +0200

    added state and first overlay idea

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,16 @@ import "./ColorBox.css"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 
 class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+         this.state = { copied: false }
+    }
     render() {
         const {name, color} = this.props;
         return (
             <CopyToClipboard text={color}>
                 <div className="ColorBox" style={{ backgroundColor: color }}>
+                    <div className="ColorBox-overlay" style={{backgroundColor: color}} />
                     <div className="ColorBox-copy-container">
                         <div className="ColorBox-content">
                             <span className="ColorBox-name">{name}</span>
