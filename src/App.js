f7f5eae12197e5accf8053caae8af4f33567d108
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-08-05 23:08:50 +0200

    added helper for setting colorRanges

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React, { Component } from 'react'
 import Palette from "./Palette"
 import './App.css';
 import seedPalettes from "./seedPalettes"
+import {generatePalette} from "./colorHelpers"
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedPalettes[1]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedPalettes[1]))
+    return (
+      <div className="App">
+        <Palette {...seedPalettes[1]} />
+      </div>
+    );
+  }
 }
 
 export default App;
