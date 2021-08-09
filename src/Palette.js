e238b67bd72d82b4a0edfbdd5be8d3e278ef3605
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-08-09 19:41:57 +0200

    +slider component for lightLevel

--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react'
-import "./Palette.css"
-import ColorBox from "./ColorBox"
+import React, { Component } from 'react';
+import './Palette.css';
+import ColorBox from './ColorBox';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
-    render() {
-        const colorBoxes = this.props.colors.map((color, idx) => <ColorBox {...color} key={idx} />)
-        return (
-            <div className="Palette">
-                
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-            </div>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = { level: 500 };
+		this.changeLevel = this.changeLevel.bind(this);
+		this.wrapper = React.createRef();
+	}
+
+	changeLevel(level) {
+		this.setState({ level });
+	}
+	render() {
+		const { level } = this.state;
+		const { colors } = this.props;
+		const colorBoxes = colors[level].map((color, idx) => (
+			<ColorBox background={color.hex} name={color.name} key={idx} />
+		));
+
+		return (
+			<div className='Palette'>
+				<Slider
+					ref={this.wrapper}
+					min={100}
+					max={900}
+					step={100}
+					defaultValue={this.state.level}
+					onAfterChange={this.changeLevel}
+				/>
+				<div className='Palette-colors'>{colorBoxes}</div>
+			</div>
+		);
+	}
 }
 
-
-export default Palette
+export default Palette;
