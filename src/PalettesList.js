3a7c5fbb7b800740dc9e6328b4af852ed87cc2ea
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-08-21 20:59:55 +0200

    added first try of inline styling

--- src/PalettesList.js
+++ src/PalettesList.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import "./PalettesList.css"
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import './PalettesList.css';
+import MiniPalette from './MiniPalette';
+
+const styles = {
+	root: {
+		backgroundColor: 'blue',
+		width: '100%',
+		height: '100vh',
+        paddingTop: '1.5rem',
+        overflow: "hidden",
+	},
+	container: {
+		width: '50%',
+		margin: '0 auto',
+		position: 'realtive',
+	},
+	nav: {
+		color: 'white',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		'& span': {
+			fontSize: '1.5rem',
+		},
+		'& a': { color: 'white' },
+	},
+};
 
 export class PalettesList extends Component {
-    render() {
-        const palettes = this.props.palettes.map(palette => (
-            <Link to={`/palette/${palette.id}`} key={palette.id}>{palette.paletteName}</Link>
-				));
-        return (
-            <div className="PalettesList">
-                {palettes}
-            </div>
-        )
-    }
+	render() {
+		const { classes } = this.props;
+
+		return (
+			<div className={classes.root}>
+				<div className={classes.container}>
+					<nav className={classes.nav}>
+						<span>React Colors</span>
+						<Link to='/'>Create Palette</Link>
+					</nav>
+					{this.props.palettes.map(palette => (
+						<MiniPalette {...palette} key={palette.id} />
+					))}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default PalettesList
+export default withStyles(styles)(PalettesList);
