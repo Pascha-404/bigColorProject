742c8748303440ddda3d43b28fd85c930fa798b5
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-08-22 22:16:43 +0200

    added structure and first style

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,18 +3,48 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = {
 	root: {
-		backgroundColor: 'black',
-		width: '30px',
-		height: '30px',
+		width: '100%',
+		// height: '150px',
+		height: "100%",
+		backgroundColor: 'white',
+		display: 'inline-block',
+		borderRadius: '5px',
+		padding: "0.4rem",
 	},
+	colors: {
+		backgroundColor: 'grey',
+		width: "100%",
+		height: "100%",
+	},
+	miniColorBoxes: {
+		width: "20%",
+		height: "25%",
+		display: "inline-block",
+	},
+	title: {
+		width: '100%',
+	},
+	emoji: {},
 };
 
 function MiniPalette(props) {
-	console.log(props);
 	const { classes, emoji, colors, paletteName } = props;
 	return (
 		<div className={classes.root}>
-			<h2>MiniPalette</h2>
+			<div className={classes.colors}>
+				{colors.map(color => {
+					return (
+						<div
+							className={classes.miniColorBoxes}
+							key={color.color}
+							style={{ backgroundColor: color.color }}></div>
+					);
+				})}
+			</div>
+			<div className={classes.title}>
+				{paletteName}
+				<span className={classes.emoji}>{emoji}</span>
+			</div>
 		</div>
 	);
 }
