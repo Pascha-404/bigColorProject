8603c2fda6a849bf6f85d372f2cdb362ed09944e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-08-29 16:55:13 +0200

    added box for going back

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,27 +1,111 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import ColorBox from './ColorBox';
 import { withStyles } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
 
 const styles = {
-	root: {},
+	root: {
+		height: '100vh',
+		width: '100%',
+		overflow: 'hidden',
+	},
+	colorBoxes: {
+		height: '90%',
+	},
+	footer: {
+		backgroundColor: '#fff',
+		width: '100%',
+		height: '4vh',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		fontWeight: 'bold',
+	},
+	emoji: {
+		margin: '0 1rem',
+		fontSize: '1.5rem',
+	},
+	goBackBox: {
+		width: '20%',
+		height: '50%',
+		display: 'inline-block',
+		position: 'relative',
+		margin: '0 auto',
+		marginBottom: '-3px',
+		backgroundColor: '#000',
+		'& a': {
+			textDecoration: 'none',
+		},
+	},
+	goBackBtn: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		textAlign: 'center',
+		width: '5rem',
+		height: '2rem',
+		position: 'absolute',
+		left: '50%',
+		top: '50%',
+		marginLeft: '-2.5rem',
+		marginTop: '-1rem',
+		border: 'none',
+		transition: 'opacity 0.5s ease',
+		cursor: 'pointer',
+		color: '#fff',
+		backgroundColor: 'rgba(255, 255, 255, 0.3)',
+		fontSize: '1rem',
+		outline: 'none',
+		textTransform: 'uppercase',
+		'&:active': {
+			transform: 'scale(0.98)',
+		},
+	},
 };
 
 export class SingleColorPalette extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { format: 'hex' };
-
+		this._shades = this.gatherShades(this.props.palette, this.props.colorId);
 		this.handleChangeFormat = this.handleChangeFormat.bind(this);
 	}
 
 	handleChangeFormat(e) {
 		this.setState({ format: e.target.value });
 	}
+
+	gatherShades(palette, colorToFilter) {
+		let shades = [];
+		let allColors = palette.colors;
+		for (let key in allColors) {
+			shades = shades.concat(allColors[key].filter(color => color.id === colorToFilter));
+		}
+		return shades.slice(1);
+	}
+
 	render() {
-		const { classes } = this.props;
+		const { format } = this.state;
+		const { classes, palette, match} = this.props;
+		const colorBoxes = this._shades.map((color, idx) => (
+			<ColorBox name={color.name} background={color[format]} key={color.name + idx} />
+		));
 		return (
 			<div className={classes.root}>
-				<Navbar format={'hex'} handleChangeFormat={this.handleChangeFormat} />
+				<Navbar format={format} handleChangeFormat={this.handleChangeFormat} />
+
+				<div className={classes.colorBoxes}>
+					{colorBoxes}
+					<div className={classes.goBackBox}>
+						<Link className={classes.goBackBtn} to={`/palette/${match.params.paletteId}`}>Go Back</Link>
+					</div>
+				</div>
+
+				<footer className={classes.footer}>
+					{palette.paletteName}
+					<span className={classes.emoji}>{palette.emoji}</span>
+				</footer>
 			</div>
 		);
 	}
