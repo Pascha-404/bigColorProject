6d7e2fc43fdfb46b454c60e30b5660781597280e
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-09-05 21:14:12 +0200

    created PaletteFooter component with styles

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,17 @@
+const styles = {
+	footer: {
+		backgroundColor: '#fff',
+		width: '100%',
+		height: '4vh',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		fontWeight: 'bold',
+	},
+	emoji: {
+		margin: '0 1rem',
+		fontSize: '1.5rem',
+	},
+};
+
+export default styles;
