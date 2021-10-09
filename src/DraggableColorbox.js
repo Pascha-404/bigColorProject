a90dfddbc0728c7b421d104f2b3395b599e99a12
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-10-09 18:23:31 +0200

    reduced code

--- src/DraggableColorbox.js
+++ src/DraggableColorbox.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
-import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from './styles/DraggableColorboxStyles';
 
 class DraggableColorbox extends Component {
-    render() {
-	const { classes, name } = this.props;
-	return (
-		<div className={classes.root}>
-			<div className={classes.content}>
-				<span className={classes.name}>{name}</span>
-				<div className={classes.deleteBtn}>
-					<IconButton aria-label='delete' disabled color='primary'>
-						<DeleteIcon />
-					</IconButton>
+	render() {
+		const { classes, name } = this.props;
+		return (
+			<div className={classes.root}>
+				<div className={classes.content}>
+					<span className={classes.name}>{name}</span>
+					<DeleteIcon className={classes.deleteBtn} />
 				</div>
 			</div>
-		</div>
-	);
-}}
+		);
+	}
+}
 
 export default withStyles(styles)(DraggableColorbox);
