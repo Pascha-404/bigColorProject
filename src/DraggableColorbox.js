956bd9d25f6befe57a3a7e5c0c65c094dab7bda0
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-10-14 19:43:40 +0200

    changed component to a sortableElement

--- src/DraggableColorbox.js
+++ src/DraggableColorbox.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { SortableElement } from 'react-sortable-hoc';
 import styles from './styles/DraggableColorboxStyles';
 
-class DraggableColorbox extends Component {
-	constructor(props) {
-		super(props);
-		this.handleClick = this.handleClick.bind(this)
-	}
-	handleClick(e) {
-		this.props.deleteColor(e)
-	}
-	render() {
-		const { classes, name} = this.props;
-		return (
-			<div className={classes.root}>
-				<div className={classes.content}>
-					<span className={classes.name}>{name}</span>
-					<DeleteIcon className={classes.deleteBtn} onClick={e => this.handleClick(name)}/>
-				</div>
+const DraggableColorbox = SortableElement((props => {
+	const { classes, name, deleteColor } = props;
+	return (
+		<div className={classes.root}>
+			<div className={classes.content}>
+				<span className={classes.name}>{name}</span>
+				<DeleteIcon className={classes.deleteBtn} onClick={e => deleteColor(name)} />
 			</div>
-		);
-	}
-}
+		</div>
+	)
+}));
+
 
 export default withStyles(styles)(DraggableColorbox);
