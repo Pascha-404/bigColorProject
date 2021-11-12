7e4836b799213613188c810cf99cc338b08bbbe7
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-11-13 00:21:20 +0100

    added functionality to component and moved code

--- src/FormDialog.js
+++ src/FormDialog.js
@@ -1,48 +1,76 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 class FormDialog extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { open: true }
-        this.handleClose = this.handleClose.bind(this)
-    }
+	constructor(props) {
+		super(props);
+		this.state = { open: true, paletteName: '' };
+		this.handleClose = this.handleClose.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    handleClose() {
-        
-    }
+	componentDidMount() {
+		// custom rule will have name 'isPaletteNameUnique'
+		ValidatorForm.addValidationRule('isPaletteNameUnique', value =>
+			this.props.palettes.every(
+				palette =>
+					palette.paletteName.toLowerCase() !== this.state.paletteName.toLowerCase()
+			)
+		);
+	}
 
-    render() {
-        const {open} = this.state
+	handleClose() {
+		this.props.hideForm();
+	}
+
+	handleChange(evt) {
+		this.setState({ [evt.target.name]: evt.target.value });
+	}
+
+	handleSubmit(evt) {
+		this.props.handleSubmit(this.state.paletteName);
+	}
+
+	render() {
+		const { open, paletteName } = this.state;
+
 		return (
 			<div>
 				<Dialog open={open} onClose={this.handleClose}>
-					<DialogTitle>Subscribe</DialogTitle>
-					<DialogContent>
-						<DialogContentText>
-							To subscribe to this website, please enter your email address here. We will
-							send updates occasionally.
-						</DialogContentText>
-						<TextField
-							autoFocus
-							margin='dense'
-							id='name'
-							label='Email Address'
-							type='email'
-							fullWidth
-							variant='standard'
-						/>
-					</DialogContent>
-					<DialogActions>
-						<Button onClick={this.handleClose}>Cancel</Button>
-						<Button onClick={this.handleClose}>Subscribe</Button>
-					</DialogActions>
+					<DialogTitle>Choose a Palette Name</DialogTitle>
+					<ValidatorForm onSubmit={this.handleSubmit}>
+						<DialogContent>
+							<DialogContentText>
+								Enter a name for your new palette. It must be unique!
+							</DialogContentText>
+
+							<TextValidator
+								autoFocus
+								margin='dense'
+								fullWidth
+								label='Palette Name'
+								value={paletteName}
+								onChange={this.handleChange}
+								name='paletteName'
+								validators={['required', 'isPaletteNameUnique']}
+								errorMessages={[
+									'Palette Name is required',
+									'Palette Name is already taken',
+								]}
+							/>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={this.handleClose}>Cancel</Button>
+							<Button type='submit'>Save Palette</Button>
+						</DialogActions>
+					</ValidatorForm>
 				</Dialog>
 			</div>
 		);
